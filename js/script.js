674264d9c5fe66d56fe67a527de66720909957f1
Dashawn <dharrell@westinghousehs.org>
2018-01-22 22:01:46 +0000

    tried new one

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+/*global $*/
 //GLOBAL VARIABLES
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var choices = ["rock", "paper", "scissors"];
 
-
-
-
-// DOCUMENT READY FUNCTION BELOW
-
+//FUNCTIONS
+$("#button").click(function(){
+         userChoice = $("#input").val();
+        $("#user").html(userChoice)
+        computerChoice = randomComputer()
+        $("#computer").html(computerChoice)
+        if(userChoice === "rock" && computerChoice === "rock"){
+                $("#winner").html("No one wins");
+        } else if(userChoice === "rock" && computerChoice === "paper"){
+                 $("#winner").html("Computer Wins!!");
+        } else if(userChoice === "rock" && computerChoice === "scissors"){
+                 $("#winner").html("User Wins!!");
+        } else if(userChoice === "paper" && computerChoice === "rock"){
+                $("#winner").html("User Wins!!");
+        } else if(userChoice === "paper" && computerChoice === "paper"){
+                $("#winner").html("No one wins");
+        } else if(userChoice === "paper" && computerChoice ==="scissors"){
+                $("#winner").html("Computer Wins");
+        } else if(userChoice === "scissors" && computerChoice === "rock"){
+                $("#winner").html("Computer Wins");
+        } else if(userChoice === "scissors" && computerChoice === "paper"){
+                $("#winner").html("User Wins");
+        } else if(userChoice === "scissors" && computerChoice === "scissors"){
+                $("#winner").html("No one Wins");
+        }
+});
+function randomComputer(){
+        return choices[Math.floor(Math.random() * 3)];
+}
